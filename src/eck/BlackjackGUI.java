3343f7cebd01f2640de937869aca2889e5f699c6
pierronline <pierre.onlinesub@gmail.com>
2013-08-05 09:29:22 +0200

    listeners are now anonymous

--- src/eck/BlackjackGUI.java
+++ src/eck/BlackjackGUI.java
@@ -1,11 +1,10 @@
 package eck;
 
-// library by David Eck
-// http://math.hws.edu/eck/cs124/javanotes6/c6/ex9-ans.html
 
 
 import java.awt.*;
 import java.awt.event.*;
+
 import javax.swing.*;
 
 
@@ -14,18 +13,10 @@ import javax.swing.*;
  * computer acts as the dealer.  The user plays by clicking
  * "Hit!" and "Stand!" buttons.
  * 
- * This class defines a panel, but it also contains a main()
- * routine that makes it possible to run the program as a
- * stand-alone application.  
- * If the panel is used in an applet the size should be about 466 pixels 
- * wide and 346 pixels high.  That width is just big enough to show 
- * 2 rows of 5 cards.  The height is probably a little bigger
- * than necessary, to allow for variations in the size of buttons 
- * from one platform to another.
- * 
  * This program depends on the following classes:  Card, Hand,
- * BlackjackHand, Deck.
+ * BlackjackHand, Deck, CardPanel
  */
+
 public class BlackjackGUI extends JPanel {
    
    /**
@@ -54,10 +45,9 @@ public class BlackjackGUI extends JPanel {
    public BlackjackGUI() {
       
       setBackground( new Color(130,50,40) );
-      
       setLayout( new BorderLayout(3,3) );
       
-      CardPanel board = new CardPanel();
+      final CardPanel board = new CardPanel();
       add(board, BorderLayout.CENTER);
       
       JPanel buttonPanel = new JPanel();
@@ -65,236 +55,46 @@ public class BlackjackGUI extends JPanel {
       add(buttonPanel, BorderLayout.SOUTH);
       
       JButton hitButton = new JButton( "Hit!" );
-      hitButton.addActionListener(board);
+      hitButton.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			board.doHit();
+		}
+      });
       buttonPanel.add(hitButton);
       
       JButton standButton = new JButton( "Stand!" );
-      standButton.addActionListener(board);
+      standButton.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			board.doStand();
+		}
+      });
       buttonPanel.add(standButton);
       
       JButton newGame = new JButton( "New Game" );
-      newGame.addActionListener(board);
+      newGame.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			board.doNewGame();
+		}
+      });
       buttonPanel.add(newGame);
       
+      final JCheckBox displayCalculations = new JCheckBox("calculs");
+      displayCalculations.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			if (displayCalculations.isSelected()) {
+				System.out.println("-- checked --");
+			}
+			else {
+				System.out.println("-- unchecked --");
+			}
+		}
+      });
+      buttonPanel.add(displayCalculations);
+
       setBorder(BorderFactory.createLineBorder( new Color(130,50,40), 3) );
       
    }  // end constructor
    
    
-   
-   /**
-    * A nested class that displays the game and does all the work
-    * of keeping track of the state and responding to user events.
-    */
-   private class CardPanel extends JPanel implements ActionListener {
-      
-      Deck deck;         // A deck of cards to be used in the game.
-      
-      BlackjackHand dealerHand;   // Hand containing the dealer's cards.
-      BlackjackHand playerHand;   // Hand containing the user's cards.
-      
-      String message;  // A message drawn on the canvas, which changes
-                       //    to reflect the state of the game.
-      
-      boolean gameInProgress;  // Set to true when a game begins and to false
-                               //   when the game ends.
-      
-      Font bigFont;      // Font that will be used to display the message.
-      Font smallFont;    // Font that will be used to draw the cards.
-      
-      
-      /**
-       * The constructor creates the fonts and starts the first game.
-       * It also sets a preferred size of 460-by-310 for the panel.
-       * The paintComponent() method assumes that this is in fact the
-       * size of the panel (although it can be a little taller with
-       * no bad effect).
-       */
-      CardPanel() {
-         setPreferredSize( new Dimension(460,310) );
-         setBackground( new Color(0,120,0) );
-         smallFont = new Font("SansSerif", Font.PLAIN, 12);
-         bigFont = new Font("Serif", Font.BOLD, 16);
-         doNewGame();
-      }
-      
-      /**
-       * Respond when the user clicks on a button by calling the appropriate 
-       * method.  Note that the buttons are created and listening is set
-       * up in the constructor of the BlackjackGUI class.
-       */
-      public void actionPerformed(ActionEvent evt) {
-         String command = evt.getActionCommand();
-         if (command.equals("Hit!"))
-            doHit();
-         else if (command.equals("Stand!"))
-            doStand();
-         else if (command.equals("New Game"))
-            doNewGame();
-      }
-      
-      
-      /**
-       * This method is called when the user clicks the "Hit!" button.  First 
-       * check that a game is actually in progress.  If not, give  an error 
-       * message and exit.  Otherwise, give the user a card.  The game can end 
-       * at this point if the user goes over 21 or if the user has taken 5 cards 
-       * without going over 21.
-       */
-      void doHit() {
-         if (gameInProgress == false) {
-            message = "Click \"New Game\" to start a new game.";
-            repaint();
-            return;
-         }
-         playerHand.addCard( deck.dealCard() );
-         if ( playerHand.getBlackjackValue() > 21 ) {
-            message = "You've busted!  Sorry, you lose.";
-            gameInProgress = false;
-         }
-         else if (playerHand.getCardCount() == 5) {
-            message = "You win by taking 5 cards without going over 21.";
-            gameInProgress = false;
-         }
-         else {
-            message = "You have " + playerHand.getBlackjackValue() + ".  Hit or Stand?";
-         }
-         repaint();
-      }
-      
-      
-      /**
-       * This method is called when the user clicks the "Stand!" button.
-       * Check whether a game is actually in progress.  If it is, the game 
-       * ends.  The dealer takes cards until either the dealer has 5 cards 
-       * or more than 16 points.  Then the  winner of the game is determined. 
-       */
-      void doStand() {
-         if (gameInProgress == false) {
-            message = "Click \"New Game\" to start a new game.";
-            repaint();
-            return;
-         }
-         gameInProgress = false;
-         while (dealerHand.getBlackjackValue() <= 16 && dealerHand.getCardCount() < 5)
-            dealerHand.addCard( deck.dealCard() );
-         if (dealerHand.getBlackjackValue() > 21)
-            message = "You win!  Dealer has busted with " + dealerHand.getBlackjackValue() + ".";
-         else if (dealerHand.getCardCount() == 5)
-            message = "Sorry, you lose.  Dealer took 5 cards without going over 21.";
-         else if (dealerHand.getBlackjackValue() > playerHand.getBlackjackValue())
-            message = "Sorry, you lose, " + dealerHand.getBlackjackValue()
-                        + " to " + playerHand.getBlackjackValue() + ".";
-         else if (dealerHand.getBlackjackValue() == playerHand.getBlackjackValue())
-            message = "Sorry, you lose.  Dealer wins on a tie.";
-         else
-            message = "You win, " + playerHand.getBlackjackValue()
-                        + " to " + dealerHand.getBlackjackValue() + "!";
-         repaint();
-      }
-      
-      
-      /**
-       * Called by the constructor, and called by actionPerformed() if  the 
-       * user clicks the "New Game" button.  Start a new game.  Deal two cards 
-       * to each player.  The game might end right then  if one of the players 
-       * had blackjack.  Otherwise, gameInProgress is set to true and the game 
-       * begins.
-       */
-      void doNewGame() {
-         if (gameInProgress) {
-               // If the current game is not over, it is an error to try
-               // to start a new game.
-            message = "You still have to finish this game!";
-            repaint();
-            return;
-         }
-         deck = new Deck();   // Create the deck and hands to use for this game.
-         dealerHand = new BlackjackHand();
-         playerHand = new BlackjackHand();
-         deck.shuffle();
-         dealerHand.addCard( deck.dealCard() );  // Deal two cards to each player.
-         dealerHand.addCard( deck.dealCard() );
-         playerHand.addCard( deck.dealCard() );
-         playerHand.addCard( deck.dealCard() );
-         if (dealerHand.getBlackjackValue() == 21) {
-            message = "Sorry, you lose.  Dealer has Blackjack.";
-            gameInProgress = false;
-         }
-         else if (playerHand.getBlackjackValue() == 21) {
-            message = "You win!  You have Blackjack.";
-            gameInProgress = false;
-         }
-         else {
-            message = "You have " + playerHand.getBlackjackValue() + ".  Hit or stand?";
-            gameInProgress = true;
-         }
-         repaint();
-      }  // end newGame();
-      
-      
-      /**
-       * The paint method shows the message at the bottom of the
-       * canvas, and it draws all of the dealt cards spread out
-       * across the canvas.
-       */
-      public void paintComponent(Graphics g) {
-         
-         super.paintComponent(g); // fill with background color.
-         
-         g.setFont(bigFont);
-         g.setColor(Color.GREEN);
-         g.drawString(message, 10, getHeight() - 10);
-         
-         // Draw labels for the two sets of cards.
-         
-         g.drawString("Dealer's Cards:", 10, 23);
-         g.drawString("Your Cards:", 10, 153);
-         
-         // Draw dealer's cards.  Draw first card face down if
-         // the game is still in progress,  It will be revealed
-         // when the game ends.
-         
-         g.setFont(smallFont);
-         if (gameInProgress)
-            drawCard(g, null, 10, 30);
-         else
-            drawCard(g, dealerHand.getCard(0), 10, 30);
-         for (int i = 1; i < dealerHand.getCardCount(); i++)
-            drawCard(g, dealerHand.getCard(i), 10 + i * 90, 30);
-         
-         // Draw the user's cards.
-         
-         for (int i = 0; i < playerHand.getCardCount(); i++)
-            drawCard(g, playerHand.getCard(i), 10 + i * 90, 160);
-         
-      }  // end paint();
-      
-      
-      /**
-       * Draws a card as a 80 by 100 rectangle with upper left corner at (x,y).
-       * The card is drawn in the graphics context g.  If card is null, then
-       * a face-down card is drawn.  (The cards are rather primitive!)
-       */
-      void drawCard(Graphics g, Card card, int x, int y) {
-         if (card == null) {  
-               // Draw a face-down card
-            g.setColor(Color.blue);
-            g.fillRect(x+10, y + 10,65,90);
-            g.setColor(Color.white);
-            g.drawRect(x+10, y + 10, 65,90);
-         }
-         else {
-			// image generation
-			String filename = "media/cards_gif/" + card.getCardToDisplay() + ".gif";
-			ImageIcon imageIcon = new ImageIcon(filename);
-			Image image = imageIcon.getImage();
-			Integer height = (int) ( (double) image.getHeight(this)/2.8 );
-			Integer width =  (int) ( (double) image.getWidth(this)/2.8 ) ;
-			g.drawImage(image, x, y, width, height,this);
-         }
-      }  // end drawCard()
-      
-   } // end nested class CardPanel
    
 } // end class BlackjackGUI
